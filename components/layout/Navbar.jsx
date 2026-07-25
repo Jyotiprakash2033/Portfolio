3fe682e008815c87dfca415be2efe3e3dc7e1bb2
Jyoti prakash <jyoti.astomverse@gmail.com>
2026-07-25 17:36:02 +0530

    navbar

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -1,7 +1,19 @@
+import { navigation } from "@/data/navigation";
 export default function Navbar() {
   return (
     <nav className="p-4 border-b">
       <h1 className="text-xl font-bold">Jyoti Prakash</h1>
+      <div>
+        {navigation.map((item) => (
+          <a
+           key={item.href}
+           href={item.href}
+           className="ml-4"
+           >
+            {item.label}
+            </a>
+        ))}
+      </div>
     </nav>
   );
 }
